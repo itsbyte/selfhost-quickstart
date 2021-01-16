b2b3149e28c3868765e6f2db90e0ee26f3fee272
Emanuele <itsbytebites@tutanota.com>
2021-01-16 09:44:37 +0100

    Initialize Repo

---
+++ nextcloud/app/previews.config.php
@@ -0,0 +1,20 @@
+<?php
+$CONFIG = array (
+'enable_previews' => true,
+'enabledPreviewProviders' =>
+ array (
+    0 => 'OC\\Preview\\TXT',
+    1 => 'OC\\Preview\\MarkDown',
+    2 => 'OC\\Preview\\PDF',
+    3 => 'OC\\Preview\\Image',
+    4 => 'OC\\Preview\\Photoshop',
+    5 => 'OC\\Preview\\TIFF',
+    6 => 'OC\\Preview\\SVG',
+    7 => 'OC\\Preview\\Font',
+    8 => 'OC\\Preview\\MP3',
+    9 => 'OC\\Preview\\Movie',
+   10 => 'OC\\Preview\\MKV',
+   11 => 'OC\\Preview\\MP4',
+   12 => 'OC\\Preview\\AVI',
+ ),
+);
---
+++ nextcloud/app/proxy.config.php
@@ -0,0 +1,5 @@
+<?php
+$CONFIG = array (
+    'overwrite.cli.url' => 'https://cloud.example.com',
+    'overwriteprotocol' => 'https',
+);
